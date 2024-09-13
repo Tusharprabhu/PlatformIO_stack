c5b15adc257b80d4bd8df3ff1dd4ce2a23183113
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-09-13 14:36:40 +0530

    Add MPU6050 library dependency and initialize MPU6050 sensor

--- mpu 6050/src/main.cpp
+++ mpu 6050/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <MPU6050.h>
 
-// put function declarations here:
-int myFunction(int, int);
+MPU6050 mpu;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Wire.begin();
+  Serial.begin(9600);
+  
+  mpu.initialize();
+  mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_2);
+  mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
+  mpu.setDLPFMode(MPU6050_DLPF_BW_20);
+  
+  Serial.println("MPU6050 initialized");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Read accelerometer and gyroscope data
+  int16_t ax, ay, az;
+  int16_t gx, gy, gz;
+  
+  mpu.getAcceleration(&ax, &ay, &az);
+  mpu.getRotation(&gx, &gy, &gz);
+  
+  // Print the sensor data
+  Serial.print("Accel: ");
+  Serial.print(ax);
+  Serial.print(", ");
+  Serial.print(ay);
+  Serial.print(", ");
+  Serial.print(az);
+  
+  Serial.print(" | Gyro: ");
+  Serial.print(gx);
+  Serial.print(", ");
+  Serial.print(gy);
+  Serial.print(", ");
+  Serial.println(gz);
+  
+  delay(1000); // Delay for 1 second
 }
