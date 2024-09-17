51cbce83ed2b06fae33ae52b35bba7384a423111
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-09-17 23:21:23 +0530

    Refactor main.cpp: Add WiFi functionality and web server for controlling LED

--- servo_with_voltage_regulator/src/main.cpp
+++ servo_with_voltage_regulator/src/main.cpp
@@ -1,41 +1,23 @@
-
 #include <WiFi.h>
 #include <ESP32Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
-
-// GPIO the servo is attached to
+Servo myservo;
 static const int servoPin = 13;
-
-// Replace with your network credentials
 const char* ssid     = "esp32";
 const char* password = "123123123";
-
-// Set web server port number to 80
 WiFiServer server(80);
-
-// Variable to store the HTTP request
 String header;
-
-// Decode HTTP GET value
-String valueString = String(5);
+String valueString = "5";
 int pos1 = 0;
 int pos2 = 0;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0; 
-// Define timeout time in milliseconds (example: 2000ms = 2s)
+unsigned long currentTime = 0;
+unsigned long previousTime = 0;
 const long timeoutTime = 2000;
 
 void setup() {
   Serial.begin(115200);
-
-  myservo.attach(servoPin);  // attaches the servo on the servoPin to the servo object
-
-  // Connect to Wi-Fi network with SSID and password
+  myservo.attach(servoPin);
+  currentTime = millis();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -43,7 +25,6 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
@@ -52,72 +33,57 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
-
-  if (client) {                             // If a new client connects,
+  WiFiClient client = server.available();
+  if (client) {
     currentTime = millis();
     previousTime = currentTime;
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {
       currentTime = millis();
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // Create a String variable to hold the HTML content
-            String html = "HTTP/1.1 200 OK\r\n";
-            html += "Content-type:text/html\r\n";
-            html += "Connection: close\r\n\r\n";
-            html += "<!DOCTYPE html><html>";
-            html += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
-            html += "<link rel=\"icon\" href=\"data:,\">";
-            html += "<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}";
-            html += ".slider { width: 300px; }</style>";
-            html += "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>";
-            html += "</head><body><h1>ESP32 with Servo</h1>";
-            html += "<p>Position: <span id=\"servoPos\"></span></p>";
-            html += "<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\"" + valueString + "\"/>";
-            html += "<script>var slider = document.getElementById(\"servoSlider\");";
-            html += "var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;";
-            html += "slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }";
-            html += "$.ajaxSetup({timeout:1000}); function servo(pos) { ";
-            html += "$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>";
-            html += "</body></html>";
-
-            // Send the HTML content to the client
-            client.print(html);
-            
-            //GET /?value=180& HTTP/1.1
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+            client.println(".slider { width: 300px; }</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+            client.println("</head><body><h1>ESP32 with Servo</h1>");
+            client.println("<p>Position: <span id=\"servoPos\"></span></p>");
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
+            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
+            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
+            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+            client.println("</body></html>");
             if(header.indexOf("GET /?value=")>=0) {
               pos1 = header.indexOf('=');
               pos2 = header.indexOf('&');
               valueString = header.substring(pos1+1, pos2);
-              
-              //Rotate the servo
               myservo.write(valueString.toInt());
-              Serial.println(valueString); 
-            }         
-            // The HTTP response ends with another blank line
+              Serial.println(valueString);
+            }
             client.println();
-            // Break out of the while loop
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else {
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') {
+          currentLine += c;
         }
       }
     }
-    // Clear the header variable
     header = "";
-    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
