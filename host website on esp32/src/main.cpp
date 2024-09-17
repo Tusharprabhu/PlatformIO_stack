4a56b220f0cbe39eaed3fac8c70f2ca94edf7934
Tusharprabhu <tusharprabhu2004@gmail.com>
2024-09-17 22:40:39 +0530

    Refactor main.cpp: Add WiFi functionality and web server for controlling LED

--- host website on esp32/src/main.cpp
+++ host website on esp32/src/main.cpp
@@ -1,8 +1,8 @@
 #include <WebServer.h>
 #include <WiFi.h>
 
-const char* ssid = "ESP32-Access-Point";
-const char* password = "123456789";
+const char *ssid = "ESP32-Access-Point";
+const char *password = "123456789";
 const int ledPin = 2;
 
 void handleRoot();
@@ -11,7 +11,8 @@ void handleOff();
 
 WebServer server(80);
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   WiFi.softAP(ssid, password);
@@ -26,21 +27,25 @@ void setup() {
   server.begin();
 }
 
-void loop() {
+void loop()
+{
   server.handleClient();
 }
 
-void handleRoot() {
+void handleRoot()
+{
   String html = "<!DOCTYPE html><html><head><title>ESP32 Web Server</title></head><body><h1>ESP32 Web Server</h1><p><a href=\"/on\"><button>ON</button></a></p><p><a href=\"/off\"><button>OFF</button></a></p></body></html>";
   server.send(200, "text/html", html.c_str());
 }
 
-void handleOn() {
+void handleOn()
+{
   digitalWrite(ledPin, HIGH);
   server.send(200, "text/plain", "LED turned on");
 }
 
-void handleOff() {
+void handleOff()
+{
   digitalWrite(ledPin, LOW);
   server.send(200, "text/plain", "LED turned off");
 }
